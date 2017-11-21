54b6789e70837810e8df6993f8ac30a076f14395
beyondwxin <king782515516@163.com>
2017-11-21 17:18:29 +0800

    first

---
+++ demo/test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def area(width, height):
+    return width + height
+
+
+def print_welcome(name):
+    print("welcome", name)
+
+
+print_welcome("Ruoo")
+w = 4
+h = 5
+print("width=", w, "height=", h, area(w, h))
