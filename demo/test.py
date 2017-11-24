9b96d9cd19a2f8a269263c70c214e4552419365e
beyondwxin <king782515516@163.com>
2017-11-24 10:42:45 +0800

    test

--- demo/test.py
+++ demo/test.py
@@ -1,17 +1,48 @@
 #!/usr/bin/python3
 
+# def area(width, height):
+#     '''计算那个面积'''
+#     return width + height
 
-def area(width, height):
-    '''计算那个面积'''
-    return width + height
+# def print_welcome(name):
+#     '''名字'''
+#     print("welcome", name)
+
+# print_welcome("Ruoo")
+# w = 4
+# h = 5
+# print("width=", w, "height=", h, area(w, h))
+
+# i = 4
+
+# def foo(x):
+#     def bar():
+#         print(i),
+
+#     for i in x:
+#         print(i),
+#     bar()
+
+# list = ['a', '23']
+# for it in list:
+#     print(it, end=',')
+
+import sys
 
 
-def print_welcome(name):
-    '''名字'''
-    print("welcome", name)
+def fab(max):
+    n = 0
+    L = []
+    while n < max:
+        L.append(n)
+        n = n + 2
+    return L
 
 
-print_welcome("Ruoo")
-w = 4
-h = 5
-print("width=", w, "height=", h, area(w, h))
+f = iter(fab(10))
+
+while True:
+    try:
+        print(next(f), end='\n')
+    except StopIteration:
+        sys.exit()
